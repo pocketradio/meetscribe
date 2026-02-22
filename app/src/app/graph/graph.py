c89e81b9518fdd1273156bb017090e1209e2723e
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-22 20:26:32 +0530

    refactor : remove in-node review pause & add memory saver checkpointer and interrupt_before review

--- app/src/app/graph/graph.py
+++ app/src/app/graph/graph.py
@@ -1,6 +1,7 @@
 from langgraph.graph import StateGraph, END
 from app.graph.state import MeetingState
 from app.crew import MeetscribeCrew
+from langgraph.checkpoint.memory import MemorySaver
 
 def crew_node(state : MeetingState) -> MeetingState:
     crew = MeetscribeCrew() # init crewai crew
@@ -14,27 +15,17 @@ def crew_node(state : MeetingState) -> MeetingState:
     return state # since add_node has a return type of state
 
 def human_review_node(state : MeetingState) -> MeetingState:
-    print(f"\n=== EMAIL DRAFT ===\n{state['email']}\n") # shows email draft to the user
-    approval = input("Approve? Enter a number : (1. Yes \n2. Edit draft \n3. Regenerate \n4. Stop): ")
-    
-    if approval == "1":
-        state['decision'] = "send"
-    elif approval == "2":
-        new_draft = input("Enter updated email:\n")
-        state["email"] = new_draft
-        state['decision'] = "edit"
-        
-    elif approval == "3":
-        state['decision'] = "regenerate"
-    else:
-        state['decision'] = "stop"
-    
+    # pausing for human review through UI
+    # stream() ends here and control returns to streamlit before continuing stream from there. 
+
     return state
         
     
 def handle_decision(state : MeetingState): # not a node since it doesn't perform any state change
-    return state['decision']
+    
+    return state.get("decision", "review")
 
+    #interrupt will protect first run but "review" is kept as a defense. wont crash mostly.
 
 
 # building the graph:
@@ -53,4 +44,8 @@ graph.add_conditional_edges("review", handle_decision, {
     "stop" : END
 })
 
-app = graph.compile()
+checkpointer = MemorySaver()
+app = graph.compile(
+    checkpointer,
+    interrupt_before=['review'] # ie. pause before every review node 
+)
