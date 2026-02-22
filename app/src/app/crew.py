ae8f2ddb1a3ddb27f3994705d104fe1a6ddfae79
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-22 10:52:53 +0530

    add meetscribe crew with agents, tasks and seq process

--- app/src/app/crew.py
+++ app/src/app/crew.py
@@ -2,63 +2,67 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from app.utils.bedrock import get_llm
 
 @CrewBase
-class App():
-    """App crew"""
+class MeetscribeCrew():
+    """Meetscribe crew"""
 
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    @agent
+    def summarizer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['summarizer'], 
+            verbose=True,
+            llm="bedrock/us.meta.llama3-2-90b-instruct-v1:0"
+        )
+
+    @agent
+    def action_item_extractor(self) -> Agent:
+        return Agent(
+            config=self.agents_config['action_item_extractor'], 
+            verbose=True,
+            llm="bedrock/us.meta.llama3-2-90b-instruct-v1:0"
+        )
+
+    @agent
+    def email_drafter(self) -> Agent:
+        return Agent(
+            config=self.agents_config['email_drafter'],
+            verbose=True,
+            llm="bedrock/us.meta.llama3-2-90b-instruct-v1:0"
+        )
     
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
-    @agent
-    def researcher(self) -> Agent:
-        return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
-
-    @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
-
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
-    def research_task(self) -> Task:
+    def summary_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['summary_task'],
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def action_items_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config['action_items_task'],
+        )
+
+    @task
+    def email_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['email_task'],
+            output_file='follow_up_email.md'
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the App crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
+        
+
+# process.seq -> each agent runs one after the other ( ie. tasks run in order since they're assigned to each agent in tasks.yaml )
+# Outputs do NOT get overwritten, they are stored in results array one after the other. results[0].raw for eg. 
